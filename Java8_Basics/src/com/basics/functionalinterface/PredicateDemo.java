d65cd8b8f60c5e5b58c007d5cb6717286d6e41ad
unknown <cogjava4572@iiht.tech>
2022-11-29 15:20:38 +0530

    added predicate demo class

---
+++ Java8_Basics/src/com/basics/functionalinterface/PredicateDemo.java
@@ -0,0 +1,45 @@
+package com.basics.functionalinterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+//public class PredicateDemo implements Predicate<Integer> {
+//
+//	@Override
+//	public boolean test(Integer t) {
+//		
+//		if(t%2==0) {
+//			return true;
+//		}	
+//		return false;
+//	}
+//	public static void main(String[] args) {
+//		
+//		Predicate<Integer> predicate = new PredicateDemo();
+//		predicate.test(35);
+//		
+//	}
+//
+//}
+
+public class PredicateDemo {
+
+	public static void main(String[] args) {
+
+		//Predicate<Integer> predicate = (t) -> t % 2 == 0;
+//		{
+//			if (t%2==0) {
+//				return true;
+//			}
+//			return false;
+//		};
+		//System.out.println(predicate.test(6898));
+
+		List<Integer> numberList = Arrays.asList(1, 2, 3, 4, 5);
+
+		//numberList.stream().filter(predicate).forEach(number -> System.out.println("Printing : " + number));
+		
+		numberList.stream().filter( (t) -> t % 2 == 0).forEach(number -> System.out.println("Printing : " + number));
+	}
+}
