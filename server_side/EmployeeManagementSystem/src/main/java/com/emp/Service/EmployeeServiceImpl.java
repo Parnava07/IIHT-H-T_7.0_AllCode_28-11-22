e56baf58e4583c1fd4ff0bd97eba21e35467e70c
unknown <cogjava4572@iiht.tech>
2022-12-06 17:52:03 +0530

    added request in controller for EMS

--- server_side/EmployeeManagementSystem/src/main/java/com/emp/Service/EmployeeServiceImpl.java
+++ server_side/EmployeeManagementSystem/src/main/java/com/emp/Service/EmployeeServiceImpl.java
@@ -1,5 +1,7 @@
 package com.emp.Service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.emp.Entity.Employee;
@@ -15,6 +17,12 @@ public class EmployeeServiceImpl implements IEmployeeService{
 	public Integer saveEmployee(Employee employee) {
 		Employee savedEmployee = employeeRepo.save(employee);
 		return savedEmployee.getId();
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		
+		return employeeRepo.findAll();
 	}
 	
 	
