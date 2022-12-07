2f9a564652136edea889cd1922c3b504015e04e7
unknown <cogjava4572@iiht.tech>
2022-12-07 15:43:56 +0530

    added crud operation

--- server_side/EmployeeManagementSystem/src/main/java/com/emp/Service/IEmployeeService.java
+++ server_side/EmployeeManagementSystem/src/main/java/com/emp/Service/IEmployeeService.java
@@ -1,6 +1,7 @@
 package com.emp.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.emp.Entity.Employee;
 
@@ -9,5 +10,10 @@ public interface IEmployeeService {
 	Integer saveEmployee(Employee employee);
 	
 	public List<Employee> getAllEmployees();
-
+	
+	Optional<Employee> getEmployee(Integer id);
+	
+	public void deleteEmployee(Integer id);
+	
+	Employee updateEmployee(Employee employee,Integer id);
 }
