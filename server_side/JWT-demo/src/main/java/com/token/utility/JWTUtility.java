514199cb396b08b37b2257edf5d1c888a8db4915
unknown <cogjava4572@iiht.tech>
2022-12-09 18:00:37 +0530

    added jwt

--- server_side/JWT-demo/src/main/java/com/token/utility/JWTUtility.java
+++ server_side/JWT-demo/src/main/java/com/token/utility/JWTUtility.java
@@ -16,38 +16,38 @@ import io.jsonwebtoken.SignatureAlgorithm;
 public class JWTUtility implements Serializable {
 
 	private static final long serialVersionUID = 1854859732857237L;
-	
-	private static final long JWT_TOKEN_VALIDITY = 5*60*60;
-	
+
+	private static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
 	private String secretKey = "Parnava";
-	
-	//to retrieve username from JWT token
+
+	// to retrieve username from JWT token
 	public String getUsernameFromToken(String token) {
 		return getClaimFromToken(token, Claims::getSubject);
 	}
-	
-	//to retrieve the expiration date from the token
+
+	// to retrieve the expiration date from the token
 	public Date getExpirationDateFromToken(String token) {
 		return getClaimFromToken(token, Claims::getExpiration);
 	}
-	
-	private <T> T getClaimFromToken(String token,Function<Claims, T> claimsResolver) {
+
+	private <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
 		final Claims claims = getAllClaimsFromToken(token);
 		return claimsResolver.apply(claims);
 	}
-	
-	//for retrieving any information from the token we wil need the secret key
+
+	// for retrieving any information from the token we wil need the secret key
 	private Claims getAllClaimsFromToken(String token) {
 		return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
 	}
-	
-	//method to check if the token is expired
+
+	// method to check if the token is expired
 	private Boolean isTokenExpired(String token) {
 		final Date expiration = getExpirationDateFromToken(token);
 		return expiration.before(new Date());
-		}
-	
-	//generate token for user
+	}
+
+	// generate token for user
 	public String generateToken(UserDetails userDetails) {
 		Map<String, Object> claims = new HashMap<>();
 		return doGenerateToken(claims, userDetails.getUsername());
@@ -57,6 +57,12 @@ public class JWTUtility implements Serializable {
 		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
 				.setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
 				.signWith(SignatureAlgorithm.HS512, secretKey).compact();
+	}
+
+	// validate the token
+	public Boolean validateToken(String token, UserDetails userDetails) {
+		final String username = getUsernameFromToken(token);
+		return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
 	}
 
 }
