dfe72fa66edc8f19921e1a632b9f6473c570804a
unknown <cogjava4572@iiht.tech>
2022-12-14 17:56:03 +0530

    added two way bindings

--- Client_side/helloworld/src/app/app.module.ts
+++ Client_side/helloworld/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { UserFormsComponent } from './components/user-forms/user-forms.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    UserFormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client_side/helloworld/src/app/components/user-forms/user-forms.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-forms',
+  templateUrl: './user-forms.component.html',
+  styleUrls: ['./user-forms.component.css']
+})
+export class UserFormsComponent implements OnInit {
+
+  title: String = 'Please Fill the below form';
+
+  firstname: String ='';
+  lastname: String ='';
+  age = 0;
+  email: String ='';
+  gender: String ='male';
+
+  save(){
+    console.log('Hello' + this.firstname + '' +
+    this.lastname);
+    console.log('Your Email Id is ' + this.email);   
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
